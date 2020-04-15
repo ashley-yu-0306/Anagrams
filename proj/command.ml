364f4037adcfee0fe5a687b7f11c71750b313efd
Bahar Tehranipoor <tehranipoorbp@gmail.com>
2020-04-15 09:10:20 -0400

    Added parse function to command

--- proj/command.ml
+++ proj/command.ml
@@ -9,4 +9,20 @@ exception Empty
 
 exception Malformed
 
-let parse str = failwith "unimplemented"
+(** [remove x acc] removes elements from the given list if [x] equals the 
+    element, returning a new list [acc] with elements not equal to [x]. *)
+let rec remove x acc = function
+  | [] -> acc
+  | h::t -> if h = x then remove x acc t else remove x (h::acc) t
+
+let parse str = 
+  if String.length str = 0 then raise Empty
+  else let lst = List.rev (String.split_on_char ' ' str |> remove "" []) in
+    if List.length lst = 0 then raise Empty 
+    else if List.nth lst 0 = "create" && List.length lst = 2 
+    then Create (List.nth lst 1)
+    else if List.nth lst 0 = "quit" && List.length lst = 1 
+    then Quit
+    else if List.nth lst 0 = "pass" && List.length lst = 1
+    then Pass
+    else raise Malformed
